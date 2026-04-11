ec37500af186283c46fda579b10a52ec3a1fff6c
AKASH YADAV <yakashadav26@gmail.com>
2026-04-11 19:35:50 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        palStr:str = ''
+        for char in s:
+            if char.isalnum():
+                palStr+=char.lower()
+        if palStr[::-1] == palStr:
+            return True
+        return False
