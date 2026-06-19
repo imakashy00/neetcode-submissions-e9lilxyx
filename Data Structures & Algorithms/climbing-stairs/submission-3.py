4eadad0e566ac447cd2cdab65f3407daf589dc85
AKASH YADAV <yakashadav26@gmail.com>
2026-06-20 02:08:25 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1: return 1
+        if n == 2: return 2
+        last, sec_last = 2, 1
+        for i in range(3, n+1):
+            last, sec_last = last+sec_last, last
+        return last
