47574b5e86af697cb1151386f529707d547723ac
AKASH YADAV <yakashadav26@gmail.com>
2026-06-24 14:57:12 +0530

    Add: longest-palindromic-substring - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def isPal(sub):
+            ind = 0
+            for _ in range(len(sub)//2):
+                if sub[ind] != sub[len(sub)-1-ind]:
+                    return False
+                ind+=1
+            return sub
+        for i in range(len(s)):
+            res = isPal(s)
+            if res:
+                return res
+            else:
+                for i in range(len(s)):
+                # The inner loop 'j' slides the starting position from left to right
+                    for j in range(i + 1):
+                    # Calculate the sliding window slice
+                    # When i=1, this checks s[0:-1] then s[1:]
+                        sub = s[j : len(s) - i + j]
+                        
+                        if isPal(sub):
+                            return sub
